b0172d838f6490e74fe185fa05df5e7ef205fe39
Suchith InterSmart <suchith@intersmart.in>
2023-10-17 17:47:54 +0530

    contact email testing

--- Modules/Frontend/Resources/views/emails/contact-mail-admin.blade.php
+++ Modules/Frontend/Resources/views/emails/contact-mail-admin.blade.php
@@ -63,13 +63,13 @@
                 <tr>
                     <td style="padding: 0px 110px 0px;">
                         <table width="380" style="margin: auto; text-align: center;">
-                            <tbody class="@getEmailLogo()">
-                                <tr class="test">
+                            <tbody>
+                                <tr>
                                     <td style="padding: 0px 0 85px;">
                                         <h1
                                             style="color: #E7151F; font-size: 20px; font-weight: 600; text-align: center; padding: 0 0 10px;margin: 0;">
-                                            Receive Contact Mail</h1>
-
+                                            Receive Contact Mai</h1>
+                                            @getEmailLogo()
                                     </td>
                                 </tr>
                             </tbody>
