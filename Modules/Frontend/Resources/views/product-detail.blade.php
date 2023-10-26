d4e8ffe92332f3e2b350eb98bb9c3a0bdcf5bfc1
Suchith InterSmart <suchith@intersmart.in>
2023-10-26 19:01:04 +0530

    product detail page notify me change

--- Modules/Frontend/Resources/views/product-detail.blade.php
+++ Modules/Frontend/Resources/views/product-detail.blade.php
@@ -225,10 +225,7 @@
                                             class="plus change-quantity-detail-page" data-product="{{ $product->slug }}">
                                     </div>
                                 </div>
-                                {{-- <div class="deliveryInfo">
-                                    Delivery by9 Sep, Saturday
-                                    <span>if ordered before 5:39 PM</span>
-                                </div> --}}
+                            @endif
                                 <div class="flxBx">
                                     <div class="priceD">
                                         <div class="cPrice">@currencySymbolWithConvertedPrice($product->price)</div>
@@ -236,11 +233,14 @@
                                             <div class="oPrice">@currencySymbolWithConvertedPrice($product->base_price)</div>
                                         @endif
                                     </div>
-                                    <div class="totalP payable-amount-div">
-                                        <div class="lbl">Payable Amount</div>
-                                        <div class="price product-total-price-div">@currencySymbolWithConvertedPrice($product->min_product_price)</div>
-                                    </div>
+                                    @if ($product->is_instock)
+                                        <div class="totalP payable-amount-div">
+                                            <div class="lbl">Payable Amount</div>
+                                            <div class="price product-total-price-div">@currencySymbolWithConvertedPrice($product->min_product_price)</div>
+                                        </div>
+                                    @endif
                                 </div>
+                            @if ($product->is_instock)
                                 <div class="expectedP">
                                     <div class="lbl">Bid Price (Per Unit):</div>
                                     <input type="text" name="bid_price" id="bid_price"
@@ -255,7 +255,7 @@
                                     </div>
                                 </div>
                                 <div class="btnBx">
-                                    <a href="javascript:void(0)" class="quote add-to-quote-product-detail"
+                                    <a href="javascript:void(0)" class="quote hoveranim add-to-quote-product-detail"
                                         data-product="{{ $product->slug }}">
                                         Add to Quote
                                         <svg viewBox="0 0 19.169 19.5">
@@ -272,9 +272,20 @@
                                 </div>
                             @else
                                 <div class="btnBx">
-                                    <a href="javascript:void(0)" class="quote notify-me" id="notify"
+                                    <a href="javascript:void(0)" class="notify notify-me hoveranim" id="notify"
                                         data-id={{ $product->slug }}>
-                                        Notify Me
+                                        <span>Notify Me</span>
+                                        <svg viewBox="0 0 100 101">
+                                            <g>
+                                                <path d="m31.0636 12.3681c.381.7356.0935 1.6409-.6421 2.0219-4.35 2.2532-8.1111 5.495-10.9812 9.465-2.8702 3.9701-4.7695 8.5579-5.5454 13.395-.1312.818-.9007 1.3747-1.7187 1.2435s-1.3747-.9007-1.2435-1.7187c.8502-5.3002 2.9314-10.3272 6.0764-14.6774 3.1449-4.3502 7.2661-7.9023 12.0326-10.3713.7356-.381 1.6408-.0936 2.0219.642z"></path>
+                                                <path d="m33.9002 17.8446c.381.7356.0936 1.6408-.642 2.0218-3.4938 1.8097-6.5146 4.4134-8.8198 7.6021-2.3053 3.1886-3.8307 6.8734-4.4539 10.7584-.1312.8179-.9007 1.3747-1.7187 1.2434-.8179-.1312-1.3746-.9006-1.2434-1.7186.6974-4.3481 2.4047-8.4721 4.9848-12.0409 2.58-3.5687 5.9609-6.4828 9.8712-8.5082.7356-.3811 1.6408-.0936 2.0218.642z"></path>
+                                                <path d="m66.7424 19.8664c-.7356-.381-1.023-1.2862-.642-2.0218s1.2863-1.0231 2.0219-.642c3.9102 2.0254 7.2911 4.9395 9.8712 8.5082 2.58 3.5688 4.2873 7.6928 4.9848 12.0409.1312.818-.4255 1.5874-1.2435 1.7186-.818.1313-1.5874-.4255-1.7186-1.2434-.6232-3.885-2.1487-7.5698-4.4539-10.7584-2.3053-3.1887-5.3261-5.7924-8.8199-7.6021z"></path>
+                                                <path d="m68.9371 12.3681c-.381.7356-.0936 1.6409.642 2.0219 4.35 2.2532 8.1111 5.495 10.9813 9.465 2.8702 3.9701 4.7695 8.5579 5.5454 13.395.1312.818.9007 1.3747 1.7186 1.2435.818-.1312 1.3747-.9007 1.2435-1.7187-.8502-5.3002-2.9313-10.3272-6.0763-14.6774s-7.2662-7.9023-12.0327-10.3713c-.7356-.381-1.6408-.0936-2.0218.642z"></path>
+                                                <path clip-rule="evenodd" d="m26.5739 46.2372v14.6765h-.2419c-2.8197 0-5.1056 2.2859-5.1056 5.1056s2.2858 5.1056 5.1056 5.1056h47.3364c2.8198 0 5.1056-2.2859 5.1056-5.1056s-2.2858-5.1056-5.1056-5.1056h-.2417v-14.6765c0-12.938-10.4884-23.4264-23.4264-23.4264-12.9381 0-23.4264 10.4884-23.4264 23.4264zm3 17.6765v-17.6765c0-11.2812 9.1452-20.4264 20.4264-20.4264 11.2811 0 20.4264 9.1453 20.4264 20.4264v17.6765h3.2417c1.1629 0 2.1056.9427 2.1056 2.1056s-.9427 2.1056-2.1056 2.1056h-47.3364c-1.1629 0-2.1056-.9427-2.1056-2.1056s.9427-2.1056 2.1056-2.1056z" fill-rule="evenodd"></path>
+                                                <path clip-rule="evenodd" d="m39.6352 76.5417c-.2816-.9731-.4254-1.983-.4254-3h21.5805c0 1.017-.1437 2.0269-.4254 3-.1108.3827-.2429.7597-.3959 1.1292-.5423 1.3092-1.3371 2.4987-2.3391 3.5006-1.0019 1.002-2.1914 1.7968-3.5006 2.3391-1.3091.5422-2.7122.8213-4.1292.8213s-2.8201-.2791-4.1293-.8213c-1.3091-.5423-2.4986-1.3371-3.5006-2.3391-1.002-1.0019-1.7968-2.1914-2.339-3.5006-.1531-.3695-.2852-.7465-.396-1.1292zm15.8734 2.5085c.7186-.7186 1.2896-1.5708 1.6809-2.5085h-14.3789c.3913.9377.9623 1.7899 1.6809 2.5085.7234.7234 1.5822 1.2972 2.5274 1.6887.9451.3915 1.9581.593 2.9812.593 1.023 0 2.036-.2015 2.9812-.593.9451-.3915 1.8039-.9653 2.5273-1.6887z" fill-rule="evenodd"></path>
+                                                <path d="m47.6478 17.1807c-.8284 0-1.5.6716-1.5 1.5s.6716 1.5 1.5 1.5h4.7053c.8284 0 1.5-.6716 1.5-1.5s-.6716-1.5-1.5-1.5z"></path>
+                                            </g>
+                                        </svg>
                                     </a>
                                 </div>
                             @endif
@@ -287,123 +298,6 @@
 
         <section id="moreInfoSec">
             <div class="container">
-                <!-- <div class="accordion" id="ProAccord">
-                                            <div class="FlxBtn">
-                                                <div class="buttonSec">
-                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                                        Description
-                                                    </button>
-                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                                        data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                                        Specifications
-                                                    </button>
-                                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                        Additional Details
-                                                    </button>
-                                                </div>
-                                            </div>
-                                            <div class="innerBx">
-                                                <div class="accordion-item">
-                                                    <div class="accordion-header" id="headingOne">
-                                                        <button class="accordion-button mobBtn collapsed" type="button" data-bs-toggle="collapse"
-                                                            data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                                            Description
-                                                        </button>
-                                                    </div>
-                                                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
-                                                        data-bs-parent="#ProAccord">
-                                                        <div class="accordion-body">
-                                                            <div class="details">
-                                                                <div class="specDtail">
-                                                                    <p>The iPhone 11 boasts a gorgeous all-screen Liquid Retina LCD that is water
-                                                                        resistant up to 2 metres for up to 30 minutes. Moreover, the ultra-wide 13
-                                                                        mm lens has a 120-degree field of view for four times more scenes, and the
-                                                                        26 mm wide lens provides up to 100% Autofocus in low light.</p>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="accordion-item">
-                                                    <div class="accordion-header" id="headingTwo">
-                                                        <button class="accordion-button mobBtn collapsed" type="button" data-bs-toggle="collapse"
-                                                            data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                                            Specifications
-                                                        </button>
-                                                    </div>
-                                                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                                                        data-bs-parent="#ProAccord">
-                                                        <div class="accordion-body">
-                                                            <div class="details">
-                                                                <div class="specBx">
-                                                                    <div class="cTitle">General</div>
-                                                                    <ul class="spec">
-                                                                        <li><span>In The Box</span>Handset, USB-C to Lightning Cable, Documentation
-                                                                        </li>
-                                                                        <li><span>Model Number</span>MPUF3HN/A</li>
-                                                                        <li><span>Model Name</span>iPhone 14</li>
-                                                                        <li><span>Color</span>Midnight</li>
-                                                                        <li><span>Browse Type</span>Smartphones</li>
-                                                                        <li><span>SIM Type</span>Dual Sim(Nano + eSIM)</li>
-                                                                        <li><span>Hybrid Sim Slot</span>No</li>
-                                                                        <li><span>Touchscreen</span>Yes</li>
-                                                                        <li><span>OTG Compatible</span>No</li>
-                                                                        <li><span>Sound Enhancements</span>Built-in Stereo Speaker</li>
-                                                                    </ul>
-                                                                    <div class="cTitle">Display Features</div>
-                                                                    <ul class="spec">
-                                                                        <li><span>Display Size</span>15.49 cm (6.1 inch)</li>
-                                                                        <li><span>Resolution</span>2532 x 1170 Pixels</li>
-                                                                        <li><span>Resolution Type</span>Super Retina XDR Display</li>
-                                                                        <li><span>GPU</span>5 Core</li>
-                                                                        <li><span>Display Type</span>Super Retina XDR Display</li>
-                                                                        <li><span>Other Display Features</span>HDR Display, True Tone, Wide Colour
-                                                                            (P3), Haptic Touch, Contrast Ratio: 20,00,000:1, Max Brightness: 800
-                                                                            nits, Peak Brightness: 1,200 nits, Fingerprint Resistant Oleophobic
-                                                                            Coating,
-                                                                            Support for Display of Multiple Languages and Characters Simultaneously
-                                                                        </li>
-                                                                    </ul>
-                                                                    <div class="cTitle">Os & Processor Features</div>
-                                                                    <ul class="spec">
-                                                                        <li><span>Operating System</span>iOS 16</li>
-                                                                        <li><span>Processor Type</span>A15 Bionic Chip, 6 Core Processor</li>
-                                                                        <li><span>Processor Core</span>Hexa Core</li>
-                                                                        <li><span>Operating Frequency</span>5G NR (Bands n1, n2, n3, n5, n7, n8,
-                                                                            n12, n20, n25, n26, n28, n30, n38, n40, n41, n48, n53, n66, n70, n77,
-                                                                            n78, n79), 4G FDD-LTE (B1, B2, B3, B4, B5, B7, B8, B12, B13, B17, B18,
-                                                                            B19, B20, B25,
-                                                                            B26, B28, B30, B32, B66), 4G TD-LTE (B34, B38, B39, B40, B41, B42, B46,
-                                                                            B48, B53), 3G UMTS/HSPA+/DC-HSDPA (850, 900, 1700/2100, 1900, 2100 MHz),
-                                                                            2G GSM/EDGE (850,
-                                                                            900, 1800, 1900 MHz)</li>
-                                                                    </ul>
-                                                                    <button type="" class="Readmore"><span>View More Details</span></button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="accordion-item">
-                                                    <div class="accordion-header" id="headingThree">
-                                                        <button class="accordion-button mobBtn collapsed" type="button" data-bs-toggle="collapse"
-                                                            data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                            Additional Details
-                                                        </button>
-                                                    </div>
-                                                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
-                                                        data-bs-parent="#ProAccord">
-                                                        <div class="accordion-body">
-                                                            <div class="details">
-
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div> -->
                 @if ($product->description)
                     <div class="title">Description</div>
                     <p>{!! $product->description !!}</p>
