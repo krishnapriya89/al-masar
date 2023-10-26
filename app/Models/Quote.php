8b501ba7f9bdda483c867fe0f1685294653d709d
Suchith InterSmart <suchith@intersmart.in>
2023-10-26 12:55:35 +0530

    quote with trashed

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -11,7 +11,7 @@ class Quote extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function product()
